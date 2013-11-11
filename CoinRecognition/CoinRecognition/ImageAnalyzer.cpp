6323604aa8a74cb91613a696671a0cc2b771b0ab
Michal Rogozinski <mjrogozinski@gmail.com>
2013-11-11 12:42:38 +0100

    adding ImageAnalyzer class to encapsulate operations on image

---
+++ CoinRecognition/CoinRecognition/ImageAnalyzer.cpp
@@ -0,0 +1,9 @@
+//
+//  ImageAnalyzer.cpp
+//  CoinRecognition
+//
+//  Created by Michal on 11/11/13.
+//  Copyright (c) 2013 Pwr. All rights reserved.
+//
+
+#include "ImageAnalyzer.h"
---
+++ CoinRecognition/CoinRecognition/ImageAnalyzer.h
@@ -0,0 +1,14 @@
+//
+//  ImageAnalyzer.h
+//  CoinRecognition
+//
+//  Created by Michal on 11/11/13.
+//  Copyright (c) 2013 Pwr. All rights reserved.
+//
+
+#ifndef __CoinRecognition__ImageAnalyzer__
+#define __CoinRecognition__ImageAnalyzer__
+
+#include <iostream>
+
+#endif /* defined(__CoinRecognition__ImageAnalyzer__) */
