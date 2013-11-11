5e8e6c5f1971a12c73674ee914948273da44257c
Michal Rogozinski <mjrogozinski@gmail.com>
2013-11-11 12:47:06 +0100

    adding ImageAnalyzer

--- CoinRecognition/CoinRecognition/ImageAnalyzer.cpp
+++ CoinRecognition/CoinRecognition/ImageAnalyzer.cpp
@@ -7,3 +7,53 @@
 //
 
 #include "ImageAnalyzer.h"
+
+ImageAnalyzer::ImageAnalyzer(const cv::Mat& image)
+: image_(image)
+{}
+
+void ImageAnalyzer::drawCircles()
+{
+    for (size_t i = 0; i < circles_.size(); i++)
+    {
+        std::cout << "Circle position x = "
+            << circles_[i][0] << ", y = "
+            << circles_[i][1] << ", radius = "
+            << circles_[i][2] << "\n";
+
+        cv::Point center(cvRound(circles_[i][0]), cvRound(circles_[i][1]));
+        int radius = cvRound(circles_[i][2]);
+
+        circle(image_, center, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
+        circle(image_, center, radius, cv::Scalar(0, 0, 255), 3, 8, 0);
+    }
+}
+
+void ImageAnalyzer::findCircles()
+{
+    cv::HoughCircles(image_, circles_, CV_HOUGH_GRADIENT, 1, 100, 100, 100, 50);
+}
+
+cv::Mat ImageAnalyzer::getChannel(int number)
+{
+    std::vector<cv::Mat> channels;
+    cv::split(image_, channels);
+
+    assert(channels.size() > number);
+    return channels[number];
+}
+
+void ImageAnalyzer::setImage(cv::Mat image)
+{
+    image_ = image;
+}
+
+void ImageAnalyzer::applyCvtColor(int filerCode)
+{
+    cv::cvtColor(image_, image_, filerCode);
+}
+
+cv::Mat ImageAnalyzer::getImage() const
+{
+    return image_;
+}
--- CoinRecognition/CoinRecognition/ImageAnalyzer.h
+++ CoinRecognition/CoinRecognition/ImageAnalyzer.h
@@ -10,5 +10,36 @@
 #define __CoinRecognition__ImageAnalyzer__
 
 #include <iostream>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+class ImageAnalyzer
+{
+public:
+    ImageAnalyzer(const cv::Mat& image);
+    
+    void applyCvtColor(int filerCode);
+    void drawCircles();
+    void findCircles();
+    cv::Mat getChannel(int number);
+    cv::Mat getImage() const;
+    void setImage(cv::Mat image);
+
+private:
+    std::vector<cv::Vec3f> circles_;
+    cv::Mat image_;
+};
 
 #endif /* defined(__CoinRecognition__ImageAnalyzer__) */
+
+    //equalizeHist(greyImage, greyImage);
+    //cv::threshold(greyImage, greyImage, 200, 255, cv::THRESH_BINARY);
+    
+
+    /*cv::GaussianBlur(greyImage,
+                     greyImage,
+                     cv::Size(9, 9),
+                     2,
+                     2);*/
+//matImage = equalizeIntensity(matImage);
