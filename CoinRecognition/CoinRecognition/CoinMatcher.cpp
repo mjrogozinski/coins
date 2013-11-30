0b409cfe4d46e7794cf3d1724bf805fd78c93151
Michal Rogozinski <mjrogozinski@gmail.com>
2013-11-30 20:03:26 +0100

    Template matching improvements

---
+++ CoinRecognition/CoinRecognition/CoinMatcher.cpp
@@ -0,0 +1,9 @@
+//
+//  CoinMatcher.cpp
+//  CoinRecognition
+//
+//  Created by Michal on 11/30/13.
+//  Copyright (c) 2013 Pwr. All rights reserved.
+//
+
+#include "CoinMatcher.h"
---
+++ CoinRecognition/CoinRecognition/CoinMatcher.h
@@ -0,0 +1,14 @@
+//
+//  CoinMatcher.h
+//  CoinRecognition
+//
+//  Created by Michal on 11/30/13.
+//  Copyright (c) 2013 Pwr. All rights reserved.
+//
+
+#ifndef __CoinRecognition__CoinMatcher__
+#define __CoinRecognition__CoinMatcher__
+
+#include <iostream>
+
+#endif /* defined(__CoinRecognition__CoinMatcher__) */
